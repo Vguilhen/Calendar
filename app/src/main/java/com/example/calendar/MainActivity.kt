5512beac20810c88e0c8ed43fbe292e4d09d4ae2
frohlichramon01 <frohlichramon01@gmail.com>
2022-08-06 15:40:30 -0300

    layout criado

--- app/src/main/java/com/example/calendar/MainActivity.kt
+++ app/src/main/java/com/example/calendar/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.calendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun previousMonthAction(view: View) {}
+    fun nextMonthAction(view: View) {}
 }
